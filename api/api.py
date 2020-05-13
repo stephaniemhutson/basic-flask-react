6d5803440d572fd2a6ca718f49f32acc571cd048
Stephanie Hutson <stephanie.m.hutson+work@gmail.com>
2020-05-13 16:50:15 -0700

    Flask and react run together

--- api/api.py
+++ api/api.py
@@ -1,8 +1,13 @@
-from Flask import flask
+from flask import Flask
 from time import time
 
-app = Flask(__name__)
+app = Flask(__name__, static_folder='../build', static_url_path="/")
 
-@app.route('/time')
+
+@app.route('/')
+def index():
+    return app.send_static_file('index.html')
+
+@app.route('/api/time')
 def get_time():
-    return {"time": time.time()}
+    return {"time": time()}
